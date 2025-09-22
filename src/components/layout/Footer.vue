916082210657fc891cc042ee1cd23e8a18c35776
khawpunkp <krittayos.kp@gmail.com>
2025-09-23 01:57:24 +0700

    tweak

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -12,10 +12,12 @@ const socials = [
 </script>
 
 <template>
-   <div class="footer">
-      <div class="footer__content">
-         <div class="footer__logo-side">
-            <div class="logo-side__logo-container">
+   <div
+      class="bg-brand-blue mobile:h-50 flex h-35 w-full flex-col items-center justify-center gap-4 text-white"
+   >
+      <div class="mobile:flex-col flex w-full max-w-6xl items-center justify-between gap-6">
+         <div class="flex flex-col gap-2">
+            <div class="flex items-center gap-3">
                <img :src="logo" class="h-12" />
                <div class="flex flex-col">
                   <Typography variant="BodyB">บริษัท ไวซ์ คอนเทนเนอร์ จำกัด</Typography>
@@ -27,7 +29,7 @@ const socials = [
                Copyright © 2025 WISE CONTAINER CO., LTD
             </Typography>
          </div>
-         <div class="footer__social-side">
+         <div class="flex items-center gap-6">
             <a
                v-for="(item, index) in socials"
                :href="item.link"
@@ -45,27 +47,3 @@ const socials = [
       </div>
    </div>
 </template>
-
-<style scoped>
-@import '#main.css';
-
-.footer {
-   @apply bg-brand-blue mobile:h-50 flex h-35 w-full flex-col items-center justify-center gap-4 text-white;
-}
-
-.footer__content {
-   @apply mobile:flex-col flex w-full max-w-6xl items-center justify-between gap-6;
-}
-
-.footer__logo-side {
-   @apply flex flex-col gap-2;
-}
-
-.logo-side__logo-container {
-   @apply flex items-center gap-3;
-}
-
-.footer__social-side {
-   @apply flex items-center gap-6;
-}
-</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,6 +2,6 @@
    <div
       class="mobile:min-h-[calc(100vh-200px)] -mt-20 flex min-h-[calc(100vh-140px)] w-full flex-col items-center justify-between gap-4 pt-28 pb-8"
    >
-      main
+      <p>main</p>
    </div>
 </template>
